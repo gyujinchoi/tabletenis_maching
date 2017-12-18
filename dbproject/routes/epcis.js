7fc543a06cf6b5a53c8e3b9ff7ae690a0ce12834
indal.choi <swdalu74@gmail.com>
2017-12-19 01:42:39 +0900

    for query gtin to epcis server

--- dbproject/routes/epcis.js
+++ dbproject/routes/epcis.js
@@ -3,54 +3,26 @@ var router = express.Router();
 var async = require('async');
 var http = require("http");
 
-var epcis_url = "http://gs1ap.asuscomm.com:8447/epcis/Service/Poll/SimpleEventQuery?MATCH_epc=urn:epc:id:sgtin:"
+var epcis_url = "http://gs1ap.asuscomm.com:8447/epcis/Service/Poll/SimpleEventQuery?MATCH_epc=urn:epc:id:sgtin:";
 
 
 router.get('/epcis?',function(req,res,next){
-
-
     if(req.query.gtin) {
-
-        /*
-        var options = {
-            host: 'gs1ap.asuscomm.com',
-            port: 8447,
-            path: '/epcis/Service/Poll/SimpleEventQuery?MATCH_epc=urn:epc:id:sgtin:',
-            method: 'GET',
+        var json_parse_options = {
+            object: true,
+            reversible: false,
+            coerce: false,
+            sanitize: true,
+            trim: true,
+            arrayNotation: false,
+            alternateTextNode: false
         };
-        var serverData = '';
-        
-        if(req.query.gtin)
-            options.path += req.query.gtin;
-
-        function handleResponse(response) {
-            var serverData = '';
-            response.on('data', function (chunk) {
-                serverData += chunk;
-                //res.json(serverData);
-            });
-            response.on('end', function () {
-                var parser = require('xml2json');
-                //console.log(serverData);
-                var json_s = parser.toJson(serverData);
-                console.log(json_s);
-                res.json(json_s);
-
-            });
-        }
-
-        http.request(options, function(response){
-            handleResponse(response);
-        }).end();
-
-*/
-        epcis_query_url = epcis_url + req.query.gtin;
         var request = require('sync-request');
-        var response = request('GET', epcis_query_url);
+        var response = request('GET', epcis_url + req.query.gtin);
         var parser = require('xml2json');
-        var json_str = parser.toJson(response.getBody());
-        console.log(json_str);
-        res.json(json_str);
+        var json_obj = parser.toJson(response.getBody(), json_parse_options);
+        console.log(json_obj);
+        res.json(json_obj);
     }else{
         res.status(401);
         res.json("error : epcis!");
