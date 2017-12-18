0d55acb22d563f1a33231f501794a2499100f5a0
arinsunny <seungwoo.ha@kaist.ac.kr>
2017-12-19 01:30:03 +0900

    add request epcis to epcis server

--- dbproject/routes/epcis.js
+++ dbproject/routes/epcis.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 var async = require('async');
 var http = require("http");
-//var https = require("https");
 
-//var epcis_url = "http://gs1ap.asuscomm.com:8447/epcis/Service/Poll/SimpleEventQuery?MATCH_epc=urn:epc:id:sgtin:"
+var epcis_url = "http://gs1ap.asuscomm.com:8447/epcis/Service/Poll/SimpleEventQuery?MATCH_epc=urn:epc:id:sgtin:"
 
 
 router.get('/epcis?',function(req,res,next){
@@ -12,13 +11,15 @@ router.get('/epcis?',function(req,res,next){
 
     if(req.query.gtin) {
 
+        /*
         var options = {
             host: 'gs1ap.asuscomm.com',
             port: 8447,
             path: '/epcis/Service/Poll/SimpleEventQuery?MATCH_epc=urn:epc:id:sgtin:',
             method: 'GET',
         };
-
+        var serverData = '';
+        
         if(req.query.gtin)
             options.path += req.query.gtin;
 
@@ -26,10 +27,15 @@ router.get('/epcis?',function(req,res,next){
             var serverData = '';
             response.on('data', function (chunk) {
                 serverData += chunk;
-                res.json(serverData);
+                //res.json(serverData);
             });
             response.on('end', function () {
+                var parser = require('xml2json');
                 //console.log(serverData);
+                var json_s = parser.toJson(serverData);
+                console.log(json_s);
+                res.json(json_s);
+
             });
         }
 
@@ -37,13 +43,18 @@ router.get('/epcis?',function(req,res,next){
             handleResponse(response);
         }).end();
 
-        //res.json(serverData);
-
+*/
+        epcis_query_url = epcis_url + req.query.gtin;
+        var request = require('sync-request');
+        var response = request('GET', epcis_query_url);
+        var parser = require('xml2json');
+        var json_str = parser.toJson(response.getBody());
+        console.log(json_str);
+        res.json(json_str);
     }else{
         res.status(401);
         res.json("error : epcis!");
     }
-
 });
 
 
