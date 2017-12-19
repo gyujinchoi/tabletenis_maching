2d2f3b9cf66b9a28a760b854b5bf0de369f49240
indal.choi <swdalu74@gmail.com>
2017-12-19 18:15:14 +0900

    for parsing json object to generate html

--- dbproject/routes/epcis.js
+++ dbproject/routes/epcis.js
@@ -5,6 +5,53 @@ var http = require("http");
 
 var epcis_url = "http://gs1ap.asuscomm.com:8447/epcis/Service/Poll/SimpleEventQuery?MATCH_epc=urn:epc:id:sgtin:";
 
+function pickOutObjectEventItems(event) {
+    var item_name = new Array();
+    var item_value = new Array();
+    var event_name = " - ";
+    var json_o = {};
+    if (event["bizStep"].indexOf("receiving")) {
+        event_name = "관리비";
+        item_name.push("관리비 부과 날짜");
+        item_value.push(event["eventTime"]);
+        item_name.push("관리비 금액");
+        item_value.push(event["building:extension"]["building:generalCharges"]["building:maintenanceCharges"]);
+    }
+    else if (event["bizStep"].indexOf("installing")) {
+        event_name = "건물등록";
+        item_name.push("건물 등록일자");
+        item_value.push(event["eventTime"]);
+    }
+
+    json_o["event"] = event_name;
+    json_o["iName"] = item_name;
+    json_o["iValue"] = item_value;
+    return json_o;
+}
+
+function parseObjectEvent(events) {
+    var max_i = 0;
+    var event_l = [];
+    var json_o = {};
+    json_o["ObjectEvent"] = [];
+    if (Array.isArray(events))
+        max_i = events.length;
+
+    if (max_i == 0) {
+        event_l[0] = pickOutObjectEventItems(events);
+        json_o["ObjectEvent"][0] = event_l[0];
+    }
+    else
+        for (var i =0; i < max_i; i++) {
+            event_l[i] = pickOutObjectEventItems(events[i]);
+            json_o["ObjectEvent"][i] = event_l[i];
+        }
+
+    console.log(json_o["ObjectEvent"][0]["iName"]);
+    console.log(json_o["ObjectEvent"][0]["iValue"]);
+    return json_o;
+}
+
 router.get('/epcis?',function(req,res,next){
     if(req.query.gtin) {
         var json_parse_options = {
@@ -21,7 +68,8 @@ router.get('/epcis?',function(req,res,next){
         var parser = require('xml2json');
         var json_obj = parser.toJson(response.getBody(), json_parse_options);
         var event_list = json_obj["EPCISQueryDocumentType"]["EPCISBody"]["ns3:QueryResults"]["resultsBody"]["EventList"];
-        console.log(event_list["ObjectEvent"]);
+
+        console.log(parseObjectEvent(event_list["ObjectEvent"]));
         //res.json(event_list);
         //res.send(html_str);
         res.render('epcis',  {"iNames": ["A", "B", "C"], "iValues":["a", "b", "c"]});
